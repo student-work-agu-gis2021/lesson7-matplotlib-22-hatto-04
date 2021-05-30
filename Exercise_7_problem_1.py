0a9bc33612e4accc718e3c8e60eff0f347594818
22hatto04 <>
2021-05-30 02:33:36 +0000

    I saved my plot as a PNG file into the disk.

--- Exercise_7_problem_1.py
+++ Exercise_7_problem_1.py
@@ -68,7 +68,7 @@ plt.show()
 outputfp = "my_first_plot.png"
 
 # YOUR CODE HERE 5
-
+plt.savefig("my_first_plot.png")
 # This test print statement should print the output filename of your figure
 print("Saved my first plot as:", outputfp)
 
